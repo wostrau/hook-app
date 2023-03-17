c1bd87f5d3f510222e60ca09676a6cb634425de7
wostrau <a.ostrovtsov@gmail.com>
2023-03-17 19:00:52 +0100

    Hook app #1 video lesson - DONE

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import './App.css'
 
-export const App: React.FC<AppPropsType> = ({age = 0, incrementAge}) => {
-    const buttonHandler = () => incrementAge(age)
+type AppPropsType = {
+    age: null | number
+    incrementAge: (arg: number) => void
+    changeCoords: (x: number, y: number) => void
+    styledCoords: { left: string, top: string }
+}
+
+export const App: React.FC<AppPropsType> = (props) => {
+    const {age = 0, incrementAge, styledCoords, changeCoords} = props
+
+    const buttonHandler = () => {
+        if (age) incrementAge(age)
+    }
+    const areaHandler: React.MouseEventHandler<HTMLDivElement> = (e) => {
+        changeCoords(e.clientX, e.clientY)
+    }
 
     return (
         <div className="App" style={{padding: '10px'}}>
+            coords:
+            <div
+                className={'area'}
+                onClick={areaHandler}
+            >
+                <div
+                    className={'point'}
+                    style={styledCoords}
+                ></div>
+            </div>
             age: {age}
             <button
                 onClick={buttonHandler}
@@ -12,8 +37,4 @@ export const App: React.FC<AppPropsType> = ({age = 0, incrementAge}) => {
             </button>
         </div>
     )
-}
-type AppPropsType = {
-    age: number
-    incrementAge: (arg: number) => void
 }
--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -3,15 +3,23 @@ import './App.css'
 import {App} from './App'
 
 
-export const AppContainer = (props: any) => {
+export const AppContainer = () => {
     const [age, setAge] = useState<number | null>(18)
     const incrementAge = (age: number) => setAge(age + 1)
 
+    const [coords, setCoords] = useState<{x: number, y: number}>({x: 50, y: 50})
+    const changeCoords = (x: number, y: number) => setCoords({x, y})
+    const styledCoords = {
+        left: coords.x + 'px',
+        top: coords.y + 'px'
+    }
+
     return (
         <App
-            {...props}
             age={age}
             incrementAge={incrementAge}
+            changeCoords={changeCoords}
+            styledCoords={styledCoords}
         />
     )
 }
