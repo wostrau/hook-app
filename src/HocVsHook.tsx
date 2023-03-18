e947bc9af51237c44413363f37f8b27fa8335df8
wostrau <a.ostrovtsov@gmail.com>
2023-03-18 23:31:08 +0100

    Hook app #2 video lesson - TBD / stopped at 01:02:00

--- src/HocVsHook.tsx
+++ src/HocVsHook.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react'
+import React, {ChangeEvent, useEffect, useState} from 'react'
 import './App.css'
 
 type HocVsHookPropsType = {}
@@ -10,7 +10,7 @@ export const HocVsHook: React.FC<HocVsHookPropsType> = (props) => {
     return (
         <div className="App" style={{padding: '10px'}}>
             <h1>HOC vs HOOK</h1>
-            <Select values={['Minsk', 'Grodna', 'Brest', 'Magilew']}/>
+            <Select values={['Minsk', 'Grodna', 'Brest', 'Magilew']} value={'Mordor'}/>
             <TextareaWithLimit
                 limit={100}
                 placeholder={'Comment'}
@@ -23,7 +23,15 @@ export const HocVsHook: React.FC<HocVsHookPropsType> = (props) => {
 type TextareaPropsType = { limit: number, placeholder: string, value: string }
 const TextareaWithLimit: React.FC<TextareaPropsType> = ({limit, value = '', ...props}) => {
     const [textValue, setTextValue] = useState<string>(value)
-    const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => setTextValue(e.currentTarget.value)
+    const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setTextValue(e.currentTarget.value)
+        localStorage.setItem('textareaValue', e.currentTarget.value)
+    }
+
+    useEffect(() => {
+        const localStorageValue = localStorage.getItem('textareaValue')
+        if (localStorageValue) setTextValue(localStorageValue)
+    }, [])
 
     return (
         <div style={{position: 'relative', width: '300px', height: '100px'}}>
@@ -41,14 +49,27 @@ const TextareaWithLimit: React.FC<TextareaPropsType> = ({limit, value = '', ...p
     )
 }
 
-type SelectPropsType = {
-    values: Array<string>
-}
 
-const Select: React.FC<SelectPropsType> = ({values}) => {
+type SelectPropsType = { values: Array<string>, value: string }
+const Select: React.FC<SelectPropsType> = ({values, value = ''}) => {
+    const [currentValue, setCurrentValue] = useState<string>(value)
+
+    const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+        setCurrentValue(e.currentTarget.value)
+        localStorage.setItem('selectValue', e.currentTarget.value)
+    }
+
+    useEffect(() => {
+        const localStorageValue = localStorage.getItem('selectValue')
+        if (localStorageValue) setCurrentValue(localStorageValue)
+    }, [])
+
     return (
-        <select>
-            {values.map(v => <option value={'City'}>{v}</option>)}
+        <select
+            value={currentValue}
+            onChange={onChangeHandler}
+        >
+            {values.map(v => <option value={currentValue}>{v}</option>)}
         </select>
     )
 }
